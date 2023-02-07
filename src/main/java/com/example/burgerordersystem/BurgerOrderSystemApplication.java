f80cecbfffb7258dcdb194913d861e22c3791105
JohannesThorbergsson <johannes.thorbergsson@hotmail.de>
2023-02-07 16:35:40 +0100

    Initial commit

---
+++ src/main/java/com/example/burgerordersystem/BurgerOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.burgerordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerOrderSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurgerOrderSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/burgerordersystem/BurgerOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.burgerordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerOrderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
